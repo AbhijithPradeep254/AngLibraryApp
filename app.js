93604ea92f1c695524ad63f221df9f6eed0f2700
Abhijith Pradeep <38107916+AbhijithPradeep254@users.noreply.github.com>
2021-04-20 22:40:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.get("/', (req,res) =>
 	res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS");
 
-	res.sendFile(__dirname+'/src/index.html');
+	res.sendFile('src/login.html', {root: '../AngLibrary'});
 }
 
 app.post("/signup", (req,res) =>
